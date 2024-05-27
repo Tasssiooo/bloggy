9c42c8a5019c939a321bdfa3a71aca9f7311cb05
Tasssiooo <tassioribeirofontes@gmail.com>
2024-05-26 23:15:18 -0300

    Some boilerplating :)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -22,7 +22,7 @@ module.exports = {
   ignorePatterns: ["!**/.server", "!**/.client"],
 
   // Base config
-  extends: ["eslint:recommended"],
+  extends: ["eslint:recommended", "plugin:@tanstack/eslint-plugin-query/recommended"],
 
   overrides: [
     // React
